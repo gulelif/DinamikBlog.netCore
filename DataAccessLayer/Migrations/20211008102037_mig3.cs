4bda046d5240fd5ecb7f5fc7573e3c6c1f9f4ed4
gulelif <elifgul12218@gmail.com>
2021-10-15 15:26:20 +0300

    Blog

---
+++ DataAccessLayer/Migrations/20211008102037_mig3.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BlogID",
+                table: "Commnets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commnets_BlogID",
+                table: "Commnets",
+                column: "BlogID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commnets_Blogs_BlogID",
+                table: "Commnets",
+                column: "BlogID",
+                principalTable: "Blogs",
+                principalColumn: "BlogID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commnets_Blogs_BlogID",
+                table: "Commnets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Commnets_BlogID",
+                table: "Commnets");
+
+            migrationBuilder.DropColumn(
+                name: "BlogID",
+                table: "Commnets");
+        }
+    }
+}
